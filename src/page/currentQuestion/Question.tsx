6af1d6a0d9483b990757e1238a9fd75f0bd8089c
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-05-10 09:42:30 +0530

    Feat: Rules page with question

---
+++ src/page/currentQuestion/Question.tsx
@@ -0,0 +1,72 @@
+import { useQuizContext } from "../../context/index"
+import { Topbar, Footer } from "../../component/index"
+import { abcd } from "../../utils/index"
+import "./question.css"
+import { useNavigate, Link } from "react-router-dom"
+import { JSXElementConstructor, Key, ReactElement, ReactFragment, ReactPortal, useState } from "react"
+import { DocumentData } from "firebase/firestore";
+
+
+const Question = () => {
+    const navigate = useNavigate()
+    const [currentIndex, setCurrentIndex] = useState(0)
+    const { quizState: { currentQuizzes }, quizDispatch } = useQuizContext()
+    const { question, a, b, c, d } = currentQuizzes[currentIndex] || {}
+
+    const [currentOption, setCurrentOption] = useState<any>("")
+
+    const options: DocumentData = [a, b, c, d]
+
+    const optionHandler = (eachOption: any) => {
+        setCurrentOption(eachOption)
+    }
+
+
+    const nextQuestionHandler = () => {
+        if (currentIndex === currentQuizzes.length - 1) {
+            navigate("/result")
+        }
+        setCurrentIndex(currentIndex + 1)
+        quizDispatch({ type: "SELECT_OPTION", payload: currentOption })
+    }
+
+    const prevQuestionHandler = () => {
+        setCurrentIndex(currentIndex - 1)
+        
+    }
+
+
+    return (
+        <div className="main-bar">
+            <Topbar />
+            <div className="user-dashboard">
+                {currentQuizzes?.length === 0 ? "Explore Quiz" : <div className="categories-section" id="categories-section">
+                    <h1 className="Larger-heading section-title space-between">Questions: 0{currentIndex + 1}/5</h1>
+
+                    <div className="question-wrapper">
+                        <h2 className="space-between question-title">{question} </h2>
+
+                        <ul className="q-list-wrapper">
+                            {options.map((eachOption: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined, i: Key | null | undefined) =>
+                                <Link to="" onClick={() => optionHandler(eachOption)} key={i}  className="q-list">
+
+                                    {abcd[Number(i)]}. {eachOption}
+
+                                </Link>
+                            )}
+                        </ul>
+
+                    </div>
+                    <div className="btn-wrapper">
+                        {currentIndex === 0 ? <button className="btn btn-secondary margin-r">Back</button> : <button
+                            onClick={() => prevQuestionHandler()} className="btn btn-secondary margin-r">Prev</button>}
+                        <button className="btn btn-primary" onClick={() => nextQuestionHandler()}>Next</button>
+                    </div>
+                </div>}
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export { Question }
--- src/page/rules/Rules.tsx
+++ src/page/rules/Rules.tsx
@@ -1,19 +1,22 @@
-import { Link, useParams } from "react-router-dom"
-import { Topbar, Profile, Footer } from "../../component/index"
-import "./rules.css"
- 
-const Rules = () => {
+import { Link, useParams, useNavigate } from "react-router-dom"
+import { Topbar, Footer } from "../../component/index"
 
-    const {category} = useParams()
+import "./rules.css"
+
+const Rules = () => {
+    const navigate = useNavigate()
+    const { category } = useParams()
+    const quizHandler = () => {
+        navigate("/question")
+    }
 
     return (
         <div className="main-bar">
             <Topbar />
             <div className="user-dashboard">
-                <Profile />
-
-                <div className="categories-section" id="categories-section">
-                        <h1 className="Larger-heading section-title">Quiz Rules</h1>
+                <div className="categories-section rule-section"  >
+                    <div>
+                        <h1 className="Larger-heading section-title">Quiz Rules for {category}</h1>
                         <ul className="rules">
                             <li className="rule-list text-md">The Total number of question is Five.</li>
                             <li className="rule-list text-md">Each question is of Five point.</li>
@@ -21,14 +24,16 @@ const Rules = () => {
                             <li className="rule-list text-md">If you gives all answered correctly. You'll be get Money Prize.</li>
                             <li className="rule-list text-md">If you're not score 50% of question. You'll be kick out.</li>
                         </ul>
-                        
-                        <div className="rules-btn-wrapper">
-                            <Link to="/"> 
-                            <button className="btn btn-secondary margin-r">Back</button> 
-                            </Link>
-                            <Link to="/"><button className="btn btn-primary">Start</button></Link>
-                        </div>
+
                     </div>
+
+                    <div className="rules-btn-wrapper">
+                        <Link to="/">
+                            <button className="btn btn-secondary margin-r">Quiz</button>
+                        </Link>
+                        <button className="btn btn-primary" onClick={() => quizHandler()}>Start</button>
+                    </div>
+                </div>
 
             </div>
             <Footer />
