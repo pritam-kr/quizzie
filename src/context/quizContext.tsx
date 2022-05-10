8b89d103543bdb68f1d1b1552b8434a0302c6fe7
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-05-10 09:44:53 +0530

    Context for quiz and auth also hooks for auth, hook for firebase stuff

--- src/context/quizContext.tsx
+++ src/context/quizContext.tsx
@@ -1,134 +1,82 @@
-import { useReducer, createContext, useContext, useEffect } from "react"
+import { useReducer, createContext, useContext, useEffect, useState } from "react"
 import { typeChildren } from "../allTypes/authTypes"
-import {categoriesRef, quizRef} from "../firebase/firebase"
-import { DocumentData, getDocs} from "firebase/firestore";
-
-
-type InitializeAction = {
-    type: "INITIALIZE";
-};
-
-type QuizInitialStateType = {
-    loading: boolean;
-    categories: DocumentData ;
-    quizzes: DocumentData ;
-}
-
-
-type LoadingAction = {
-    type: "SET_LOADING";
-    payload: boolean;
-}
-
-type GetCategoriesAction = {
-    type: "GET_CATEGORY";
-    payload: {
-        categories: DocumentData 
-    }
-}
-
-type GetQuizAction = {
-    type: "GET_QUIZ";
-    payload: {
-        quizzes: DocumentData 
-    }
-}
-
-
-type QuizzesActions = InitializeAction | LoadingAction | GetCategoriesAction | GetQuizAction
-
-export type QuizContextType = {
-    quizState: QuizInitialStateType;
-    quizDispatch: React.Dispatch<QuizzesActions>;
-};
+import { QuizContextType} from "..//allTypes/quizTypes"
+import { categoriesRef, db, quizRef } from "../firebase/firebase"
+import { DocumentData, getDocs, collection, onSnapshot } from "firebase/firestore";
+import {quizReducer,  initialState} from "../reducer/index"
+ 
 
 const quizContext = createContext<QuizContextType>({} as QuizContextType)
 
-
-const initialState = {
-    loading: false,
-    categories: [],
-    quizzes: []
-}
-
-
-const quizReducer = (state: QuizInitialStateType, action: QuizzesActions) => {
-
-    switch (action.type) {
-        
-        case "SET_LOADING":
-        return {...state, loading: action.payload}
-
-        case "GET_CATEGORY":
-        return{...state, categories: action.payload.categories}
-        
-        default:
-        return state;
-    }
-}
-
-
+ 
 const QuizContextProvider = ({ children }: typeChildren) => {
     const [quizState, quizDispatch] = useReducer(quizReducer, initialState)
+    const [leaderBoard, setLeaderBoard] = useState<any>([])
+
 
     // Getting categories from firebase 
     useEffect(() => {
         (async () => {
-            quizDispatch({type: "SET_LOADING", payload: true})
+            quizDispatch({ type: "SET_LOADING", payload: true })
 
             try {
                 let res = await getDocs(categoriesRef);
                 const categories: DocumentData | undefined = res.docs.map(ele => {
-                  return { ...ele.data(), id: ele.id };
+                    return { ...ele.data(), id: ele.id };
                 });
 
-              if(categories){
-                quizDispatch({type: "SET_LOADING", payload: false})
-                quizDispatch({type: "GET_CATEGORY", payload: {categories}})
+                if (categories) {
+                    quizDispatch({ type: "SET_LOADING", payload: false })
+                    quizDispatch({ type: "GET_CATEGORY", payload: { categories } })
 
-              }
-              } catch (err) {
-                quizDispatch({type: "SET_LOADING", payload: false})
+                }
+            } catch (err) {
+                quizDispatch({ type: "SET_LOADING", payload: false })
                 console.log(err);
                 throw Error("something went wrong");
-              }
+            }
         })()
 
     }, [])
 
+    //Getting leader board data
+    useEffect(() => {
+        onSnapshot(collection(db, "leaderBoard"), (snapshot) => {
+        setLeaderBoard(snapshot.docs.map((doc) => ({ ...doc.data() })))
+       })
+    }, [])
 
-      // Getting quizzes from firebase 
-      useEffect(() => {
+    // Getting quizzes from firebase 
+    useEffect(() => {
         (async () => {
-            
+
             try {
                 let res = await getDocs(quizRef);
                 const quizzes: DocumentData | undefined = res.docs.map(ele => {
-                  return { ...ele.data(), id: ele.id };
+                    return { ...ele.data(), id: ele.id };
                 });
 
-                console.log(quizzes)
-               
-              } catch (err) {
-                
+                if (quizzes) {
+                     
+                    quizDispatch({ type: "GET_QUIZ", payload: { quizzes } })
+                }
+
+            } catch (err) {
                 console.log(err);
                 throw Error("something went wrong");
-              }
+            }
         })()
 
     }, [])
 
 
     return (
-        <quizContext.Provider value={{ quizState, quizDispatch }}>
+        <quizContext.Provider value={{ quizState, quizDispatch, leaderBoard }}>
             {children}
         </quizContext.Provider>
     )
 
 }
-
-
-
 const useQuizContext = () => useContext(quizContext)
 
 export { QuizContextProvider, useQuizContext }
--- src/hooks/useFirebase.ts
+++ src/hooks/useFirebase.ts
@@ -1,22 +1,19 @@
 import {
   collection,
   doc,
-  DocumentData,
   getDoc,
-  getDocs,
+  addDoc,
   setDoc,
 } from "firebase/firestore";
-import { auth, db } from "../firebase/firebase";
+import { db } from "../firebase/firebase";
 import { User } from "firebase/auth";
 import toast from "react-hot-toast";
 
-type func = {
-  createUserCollection?: () => void;
-};
+
 
 export const useFirebase = () => {
 
-//Sending users info to firebase 
+  //Sending users info to firebase 
   const createUserCollection = async (
     user: User,
     userInfo: { firstName: string; lastName: string }
@@ -39,7 +36,7 @@ export const useFirebase = () => {
           createdAt: new Date(),
         }).then((res) => console.log(res));
       } catch (error) {
-        toast.error(`Error occured while creating user collection ${error}`, {position: "top-right"});
+        toast.error(`Error occured while creating user collection ${error}`, { position: "top-right" });
       }
     }
   };
@@ -51,23 +48,38 @@ export const useFirebase = () => {
     try {
       const userRef = doc(db, `users/${uid}`);
 
-      const snapshot:any = await getDoc(userRef);
-      const email:string = snapshot._document.data.value.mapValue.fields.email.stringValue;
-      const firstName:string = snapshot._document.data.value.mapValue.fields.firstName.stringValue;
-      const lastName:string = snapshot._document.data.value.mapValue.fields.lastName.stringValue;
+      const snapshot: any = await getDoc(userRef);
+      const email: string = snapshot._document.data.value.mapValue.fields.email.stringValue;
+      const firstName: string = snapshot._document.data.value.mapValue.fields.firstName.stringValue;
+      const lastName: string = snapshot._document.data.value.mapValue.fields.lastName.stringValue;
 
       if (snapshot) {
-        localStorage.setItem("username", JSON.stringify({email: email, firstName: firstName, lastName: lastName}));
+        localStorage.setItem("username", JSON.stringify({ email: email, firstName: firstName, lastName: lastName }));
       }
 
       if (snapshot.exists()) {
         return snapshot.data()
       }
     } catch (err) {
-      
-      toast.error("Error occured while fetching user info in useFirebase.", {position: "top-right"})
+
+      toast.error("Error occured while fetching user info in useFirebase.", { position: "top-right" })
     }
   };
 
-  return { createUserCollection, getUserInfo };
+
+  //Leader board 
+  type leaderBoardType = {
+    email: string;
+    firstName: string;
+    lastName: string;
+    score: number;
+  }
+
+  const setLeaderBoardData = async (data: leaderBoardType) => {
+    const leaderBoardData = data
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const leaderBoardRef = await addDoc(collection(db, "leaderBoard"), leaderBoardData);
+  }
+
+  return { createUserCollection, getUserInfo, setLeaderBoardData };
 };
